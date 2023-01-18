7dd69bde9fbe15d32cbff5566eec9a2fce5c93b6
Artem Oleynikov <allacrit@yandex.ru>
2023-01-18 21:19:25 +0300

    Создание решения 2-й задачи

---
+++ 02/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear(); 
+Console.WriteLine("По двум заданным числам проверять является ли первое квадратом второго.");
+Console.Write("Введите число в котором проверяем значание: ");
+string writeNumber1 = Console.ReadLine();
+int number1 = int.Parse(writeNumber1);
+
+Console.Write("Введите число которое является квадратом первого числа: ");
+string writeNumber2 = Console.ReadLine();
+int number2 = int.Parse(writeNumber2);
+
+string txtResult = $"Число {number1} не является квадратом числа {number2}";
+
+if (number1 == number2 * number2)
+{
+    txtResult = txtResult.Replace(" не", "");
+}
+Console.WriteLine(txtResult);
