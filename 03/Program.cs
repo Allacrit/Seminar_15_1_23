22bf708efd9b1b8a2c3e474cf0e3a7cafdcbf16f
Artem Oleynikov <allacrit@yandex.ru>
2023-01-18 21:45:51 +0300

    Создание решения 3-й задачи

---
+++ 03/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Задать номер четверти, показать диапазоны для возможных координат.");
+Console.Write("Введите номер четверти: ");
+int quarterNumber = int.Parse(Console.ReadLine());
+
+switch(quarterNumber)
+{
+    case 1: Console.WriteLine("Для 1-й четверти координаты точки должны быть заданы в диапазоне (x > 0, y > 0)."); break;
+    case 2: Console.WriteLine("Для 2-й четверти координаты точки должны быть заданы в диапазоне (x < 0, y > 0)."); break;
+    case 3: Console.WriteLine("Для 3-й четверти координаты точки должны быть заданы в диапазоне (x < 0, y < 0)."); break;
+    case 4: Console.WriteLine("Для 4-й четверти координаты точки должны быть заданы в диапазоне (x > 0, y < 0)."); break;
+    default: Console.WriteLine($"Число {quarterNumber} не подходит для номера четверти!"); break;
+}
